f9f67b34dd15c4f2657fc5d1d244a86d3dfc136e
checkerce <chekerce@gmail.com>
2023-10-26 08:03:21 +0200

    ability to switch interval

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ import time
 import os
 import json
 import ctypes
+import pystray
+from PIL import Image
+import threading
+
+iconUrl = "https://inspirobot.me/website/images/inspirobot-dark-green.png"
 
 apiURL = "https://inspirobot.me/api?generate=true"
 appdata = os.getenv("APPDATA")
@@ -11,6 +16,8 @@ downloadPath = os.path.join(appdata, "inspiroBackground", "background.jpg")
 configPath = os.path.join(appdata, "inspiroBackground", "config.json")
 
 interval = 60
+iconImage = Image.open(requests.get(iconUrl, stream=True).raw)
+running = True
 
 
 def loadConfig():
@@ -25,6 +32,17 @@ def loadConfig():
             interval = config["interval"]
 
 
+def saveConfig():
+    with open(configPath, "w") as configFile:
+        json.dump({"interval": interval}, configFile)
+
+
+def setInterval(newInterval):
+    global interval
+    interval = newInterval
+    saveConfig()
+
+
 def setBackground():
     # get image url
     imgURL = requests.get(apiURL).text
@@ -37,9 +55,49 @@ def setBackground():
     ctypes.windll.user32.SystemParametersInfoW(20, 0, absolutePath, 0)
 
 
-if __name__ == "__main__":
+def end():
+    icon.stop()
+    global running
+    running = False
+
+
+def isCustomInterval(x):
+    return interval not in [30, 300, 600, 900, 1800]
+
+
+icon = pystray.Icon("inspiroBackground", icon=iconImage)
+icon.menu = pystray.Menu(
+    pystray.MenuItem("new Background", setBackground),
+    # submenu to set interval
+    pystray.MenuItem("Set interval",
+                     pystray.Menu(
+                         pystray.MenuItem("30 seconds", lambda: setInterval(30), checked=lambda x: interval == 30),
+                         pystray.MenuItem("1 minute", lambda: setInterval(300), checked=lambda x: interval == 300),
+                         pystray.MenuItem("5 minutes", lambda: setInterval(600), checked=lambda x: interval == 600),
+                         pystray.MenuItem("10 minutes", lambda: setInterval(900), checked=lambda x: interval == 900),
+                         pystray.MenuItem("15 minutes", lambda: setInterval(1800), checked=lambda x: interval == 1800),
+                         pystray.MenuItem("custom", lambda: os.startfile(os.path.abspath(configPath)),
+                                          checked=isCustomInterval),
+                     )
+                     ),
+    pystray.MenuItem("reload config", loadConfig),
+    pystray.MenuItem("Exit", end)
+)
+
+
+def loop():
     loadConfig()
-    while True:
-        setBackground()
-        print("Background set.")
-        time.sleep(interval)
+    while running:
+        try:
+            setBackground()
+        except Exception as e:
+            print(e)
+            time.sleep(5)
+        lastTime = time.time()
+        while time.time() - lastTime < interval and running:
+            time.sleep(1)
+
+
+thread = threading.Thread(target=loop)
+thread.start()
+icon.run()
